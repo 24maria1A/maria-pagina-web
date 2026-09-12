1dd0b68ab54bdafe675e0ab7652cebd25659b6e1
24maria1A <maria.pescador@escola.pr.gov.br>
2026-09-12 18:50:18 -0300

    aula 8

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-    const botoes = document.querySelectorAll("button");
+const botoes = document.querySelectorAll("article button");
 
-    botoes.forEach(function (botao) {
-        let curtiu = false; 
-        botao.addEventListener("click", function botaoClicado() {
-            console.log("fui clicado");
-            let texto = botao.querySelector("span");
-            if (curtiu === false){
-                texto.textContent++;
-                curtiu = true;
-            } else{
-                texto.textContent--;
-                curtiu = false;
-            }
-        })
+botoes.forEach(function (botao) {
+    let curtiu = false; 
+    botao.addEventListener("click", function botaoClicado() {
+        console.log("fui clicado");
+        let texto = botao.querySelector("span");
+        if (curtiu === false){
+            texto.textContent++;
+            curtiu = true;
+        } else{
+            texto.textContent--;
+            curtiu = false;
+        }
     })
+})
 
-    const BtnTemaEscuro =  document.querySelector(".bnt-tema-escuro");
+const BtnTemaEscuro = document.querySelector(".btn-tema-escuro");
 
 BtnTemaEscuro.addEventListener("click", mudaTema);
 
 function mudaTema() {
     const corpoPagina = document.body;
-    if(corpoPagina.classlist.contains("tema-escuro")) {
-        corpoPagina.classlist.remove("tema-escuro");
-        } else {
-    corpoPagina.classlist.add("tema-escuro");
+    if(corpoPagina.classList.contains("tema-escuro")) {
+        corpoPagina.classList.remove("tema-escuro");
+    } else {
+        corpoPagina.classList.add("tema-escuro");
     }
 }
